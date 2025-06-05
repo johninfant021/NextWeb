f081c2e3682ddb0a475925619824908199408c68
johninfant021 <127079706+johninfant021@users.noreply.github.com>
2025-06-05 16:23:31 +0530

    second

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './Pages/Header/Header'
 import './App.css'
 import Banner from './Pages/Banner/Banner'
+import Client from './Pages/Client/Client'
 
 const App = () => {
   return (
     <div className='container'>
       <Header/>
       <Banner/>
+      <Client/>
     </div>
   )
 }
--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <div className='Banner'>
         <img src='https://cdn.pixabay.com/photo/2016/06/13/09/57/meeting-1453895_1280.png' alt='Logo'/>
-        <h2>Modern Web Solutions for your business</h2>
+        <h2 className='head-info'>Modern Web Solutions for your business</h2>
     </div>
   )
 }
